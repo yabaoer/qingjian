36d08df23d2016f03f92afedc4a9a4cc2137e3a9
YihanYu <yu.yihan@qq.com>
2026-05-31 13:37:31 +0800

    feat: 重做首页为精美登录界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,40 @@
 Page({
   data: {
-    
+    account: '',
+    password: '',
+    remember: false
   },
-  showQrcode() {
-    wx.navigateTo({
-      url: '../showQrcode/showQrcode?url=https://img10.360buyimg.com/imgzone/jfs/t1/234244/23/11388/22231/659e3c12F83582cc5/6618700217d2bab6.jpg&title=扫码加微信&desc=加微信请备注'
-    })
+
+  onAccountInput(e) {
+    this.setData({ account: e.detail.value })
   },
-  goAlbum() {
-    wx.navigateTo({
-      url: '../album/album'
-    })
+
+  onPasswordInput(e) {
+    this.setData({ password: e.detail.value })
+  },
+
+  toggleRemember() {
+    this.setData({ remember: !this.data.remember })
+  },
+
+  doLogin() {
+    const { account, password } = this.data
+    if (!account) {
+      wx.showToast({ title: '请输入账号', icon: 'none' })
+      return
+    }
+    if (!password) {
+      wx.showToast({ title: '请输入密码', icon: 'none' })
+      return
+    }
+    // 模拟登录成功，跳转相册
+    wx.showToast({ title: '登录成功', icon: 'success' })
+    setTimeout(() => {
+      wx.switchTab({ url: '../album/album' })
+    }, 800)
+  },
+
+  goRegister() {
+    wx.showToast({ title: '注册功能开发中', icon: 'none' })
   }
 })
